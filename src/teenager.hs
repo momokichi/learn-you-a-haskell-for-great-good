62f15116bf92cc78f41528e3d550e5ee7681757a
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-07-12 20:01:19 +0900

    done: section5

--- src/teenager.hs
+++ src/teenager.hs
@@ -1,3 +1,50 @@
 multThree :: Int -> Int -> Int -> Int
 multThree x y z = x*y*z
 
+compareWithHundred :: Int -> Ordering
+compareWithHundred x = compare 100 x
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+  where g x y = f y x
+
+largestDivisible :: Integer
+largestDivisible = head (filter p [100000, 99999..])
+  where p x = x `mod` 3829 == 0
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+  | even n = n : chain (n `div` 2)
+  | odd n = n : chain (n * 3 + 1)
+
+numLongChains :: Int
+-- numLongChains = length (filter isLong (map chain [1..100]))
+--   where isLong xs = length xs > 15
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
